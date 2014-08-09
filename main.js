5b43134650c5f424d5db744c0bf7d100c435d007
abrie <abrie@>
2014-08-09 14:45:28 +0100

    experiment with Dagre diagrams

--- main.js
+++ main.js
@@ -8,126 +8,40 @@ function main() {
     }); 
 }
 
-function displayData( graph ) {
-    var width = 960;
-    var height = 800;
+function displayData( graphData ) {
+    // Create a new directed graph
+    var g = new dagreD3.Digraph();
 
-    var color = d3.scale.category20();
-    var radius = d3.scale.sqrt() .range([0, 6]);
-
-    var svg = d3.select("body").append("svg");
-    svg.attr("width", width).attr("height", height);
-
-    // build the arrow.
-    svg.append("svg:defs").selectAll("marker")
-    .data(["end"])      // Different link/path types can be defined here
-    .enter()
-    .append("svg:marker")    // This section adds in the arrows
-    .attr("id", String)
-    .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
-    .attr("refY", -1.5)
-    .attr("markerWidth", 6)
-    .attr("markerHeight", 6)
-    .attr("orient", "auto")
-    .append("svg:path")
-    .attr("d", "M0,-5L10,0L0,5");
-
-    var force = d3.layout.force()
-    .size([width, height])
-    .charge(-400)
-    .linkDistance(function(d) { 
-        if( d.probability != 0 ) {
-            return 1/d.probability * 3; 
-        }
-        else {
-            return 400;
-        }
+    graphData.nodes.forEach( function(node) {
+        g.addNode(node.name, { 
+            label: node.name,
+            labelStyle: "font-size: 60em;" 
+        });
     });
 
-    force.nodes(graph.nodes);
-    force.links(graph.links);
-    force.on("tick", tick).start();
+    graphData.links.forEach( function(edge) {
+        var a = graphData.nodes[edge.source].name;
+        var b = graphData.nodes[edge.target].name;
+        var p = edge.probability;
+        var appearance = {};
+        if( p < 0.01 ) {
+            appearance = { style: 'stroke-width: 1px;' };
+        }
+        else if( p < 0.10 ) {
+            appearance = { style: 'stroke-width: 5px;' };
+        }
+        else if( p < 0.40 ) {
+            appearance = { style: 'stroke-width: 30px;' };
+        }
+        else {
+            appearance = { style: 'stroke-width: 60px;' };
+        }
+        g.addEdge( null, a,b, appearance )
+    });
 
-    var link = svg.selectAll(".link")
-    .data(graph.links)
-    .enter().append("path")
-    .attr("class","link")
-    .attr("marker-end", "url(#end)");
-
-    var node = svg.selectAll(".node")
-    .data(graph.nodes)
-    .enter().append("g")
-    .attr("class", "node")
-    .call(force.drag);
-
-    node.append("circle")
-    .attr("r", function(d) { return radius(d.size); })
-    .style("fill", function(d) { return color(d.atom); });
-
-    node.append("text")
-    .attr("x", 12)
-    .attr("dy", ".35em")
-    .text( function(d) { return d.name; } )
-
-    function linkTick(d) {
-        var x1 = d.source.x,
-        y1 = d.source.y,
-        x2 = d.target.x,
-        y2 = d.target.y,
-        dx = x2 - x1,
-        dy = y2 - y1,
-        dr = Math.sqrt(dx * dx + dy * dy),
-
-        // Defaults for normal edge.
-        drx = dr,
-        dry = dr,
-        xRotation = 0, // degrees
-        largeArc = 0, // 1 or 0
-        sweep = 1; // 1 or 0
-
-        // Self edge.
-        if ( x1 === x2 && y1 === y2 ) {
-            // Fiddle with this angle to get loop oriented.
-            xRotation = -45;
-
-            // Needs to be 1.
-            largeArc = 1;
-
-            // Change sweep to change orientation of loop. 
-            //sweep = 0;
-
-            // Make drx and dry different to get an ellipse
-            // instead of a circle.
-            drx = 30;
-            dry = 20;
-
-            // For whatever reason the arc collapses to a point if the beginning
-            // and ending points of the arc are the same, so kludge it.
-            x2 = x2 + 1;
-            y2 = y2 + 1;
-        } 
-
-        return "M" + 
-            x1 + "," + 
-            y1 + "A" + 
-            drx + "," + 
-            dry + " " + 
-            xRotation + "," + 
-            largeArc + "," + 
-            sweep + " " + 
-            x2 + "," + 
-            y2;
-    }
-
-    function nodeTick(d) {
-        return "translate(" + d.x + "," + d.y + ")"; 
-    }
-
-    function tick() {
-        link.attr("d", linkTick );
-        node.attr("transform",nodeTick ); 
-    }
+    var renderer = new dagreD3.Renderer();
+    renderer.run(g, d3.select("svg g"));
+    console.log("done.");
 }
 
 function processData( theData ) {
