0ec2a8c6f3d4ca07e369e41f97f0826c6ba0c73d
abrie <abrie@>
2014-08-10 16:56:44 +0100

    write results in two stages first to a tab delimited file suitable for processing by MCL then read the file and write in json format

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run
 
-
 # Enter your Google Developer Project number
 PROJECT_NUMBER = 'linear-quasar-662'
 
@@ -32,31 +31,50 @@ def main():
 
   bigquery_service = build('bigquery', 'v2', http=http)
 
-  with open ("query.sql", "r") as myfile:
-          query=myfile.read()
+  with open ("query.sql", "r") as query_file:
+          bql=query_file.read()
   try:
-    query_request = bigquery_service.jobs()
-    query_data = {'query':query}
+    query_job = bigquery_service.jobs()
+    query_body = {'query':bql}
+    query_request = query_job.query(projectId=PROJECT_NUMBER, body=query_body)
+    query_response = query_request.execute()
 
-    query_response = query_request.query(projectId=PROJECT_NUMBER,
-                                         body=query_data).execute()
-    print 'Query Results:'
+    while not query_response['jobComplete']:
+        query_response = query_job.getQueryResults(
+                projectId=query_response['jobReference']['projectId'],
+                jobId=query_response['jobReference']['jobId'])
+
+    print "projectId: " + query_response['jobReference']['projectId']
+    print "jobId: " + query_response['jobReference']['jobId'] 
+    print "cacheHit:" + str(query_response['cacheHit'])
+
+    with open('mcl_input', 'w') as mcl_file:
+        for row in query_response['rows']:
+          fields = row['f'];
+          first = fields[0]['v'];
+          second = fields[1]['v'];
+          count = fields[2]['v'];
+          ratio = fields[3]['v'];
+          mcl_file.write('{0}\t{1}\t{2}\n'.format(first,second,ratio))
+
+    print str( query_response['totalRows'] ) + " rows retrieved."
+
     markov_map = {}
-    for row in query_response['rows']:
-      fields = row['f'];
-      first = fields[0]['v'];
-      second = fields[1]['v'];
-      count = fields[2]['v'];
-      ratio = fields[3]['v'];
-      if not markov_map.has_key(first):
-        markov_map[first] = {}
-      markov_map[first][second] = float(ratio)
+    with open('mcl_input', 'r') as mcl_file:
+        for line in mcl_file.readlines():
+            fields = line.rstrip('\n').split('\t')
+            first = fields[0]
+            second = fields[1]
+            ratio = fields[2]
+            if not markov_map.has_key(first):
+                markov_map[first] = {}
+            markov_map[first][second] = float(ratio)
 
     with open('results.json', 'w') as outfile:
       json.dump(markov_map, outfile)
 
   except HttpError as err:
-    print 'Error:', pprint.pprint(err.content)
+    print 'HttpError:', pprint.pprint(err.content)
 
   except AccessTokenRefreshError:
     print ("Credentials have been revoked or expired, please re-run"
