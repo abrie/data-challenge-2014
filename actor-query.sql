02ca87c0d3177c78cfba5918e8efb24628749b74
abrie <abrie@>
2014-08-24 04:18:24 +0100

    actor query includes null repositories

--- actor-query.sql
+++ actor-query.sql
@@ -4,26 +4,38 @@ SELECT
 FROM(
   SELECT
     state as present_state,
-    LAG(present_state, 1, '~') OVER (PARTITION BY actor ORDER BY time) as previous_state
+    LAG(present_state, 1, '~') OVER (PARTITION BY actor ORDER BY created_at) as previous_state
   FROM(
     SELECT
     actor,
-    PARSE_UTC_USEC(created_at) as time,
+    created_at,
     CASE
       WHEN type = "CreateEvent" THEN CONCAT("CreateEvent:",
         IF(payload_ref_type is null, "repository", payload_ref_type))
       WHEN type = "DeleteEvent" THEN CONCAT("DeleteEvent:", payload_ref_type)
-      WHEN type = "PullRequestEvent" THEN CONCAT("PullRequestEvent:", payload_action)
+      WHEN type = "PullRequestEvent" THEN CONCAT("PullRequestEvent:",
+        CASE
+            WHEN payload_action = "closed" THEN
+              IF(payload_pull_request_merged == "false", "closed-not-merged","closed-merged")
+            ELSE payload_action
+        END)
       WHEN type = "IssuesEvent" THEN CONCAT("IssuesEvent:", payload_action)
       WHEN type = "GistEvent" THEN CONCAT("GistEvent:", payload_action)
-      WHEN type = "IssueCommentEvent" THEN CONCAT("IssueCommentEvent:", 
+      WHEN type = "IssueCommentEvent" THEN CONCAT("IssueCommentEvent:",
         IF(payload_action is null, "created", payload_action))
+      WHEN type = "ReleaseEvent" THEN CONCAT("ReleaseEvent:", payload_action)
+      WHEN type = "WatchEvent" THEN CONCAT("WatchEvent:", payload_action)
+      WHEN type = "MemberEvent" THEN CONCAT("MemberEvent:", payload_action)
+      WHEN type = "GollumEvent" Then CONCAT("GollumEvent:", payload_page_action)
       ELSE type
     END as state,
     FROM
-        $dataset 
+        $dataset
     WHERE
-      actor IS NOT NULL
-    GROUP EACH BY actor, time, state
-  ) GROUP EACH BY actor, time, present_state
+      type IS NOT NULL  
+      AND actor IS NOT NULL
+      AND NOT (type = "PullRequestEvent" AND payload_action = "merged")
+      AND created_at >= "2011-2-12" // the offical start date of the archive. 
+    GROUP EACH BY actor, created_at, state
+  ) GROUP EACH BY actor, created_at, present_state
 ) GROUP EACH BY previous_state, present_state
