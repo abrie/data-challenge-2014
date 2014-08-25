b83cf2fdd6260cc2eb0db570bbeec6ec5531f171
abrie <abrie@>
2014-08-25 19:08:32 +0100

    refactor page generator

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ cp -R lib ghpages
 cp main.js ghpages
 cp style.css ghpages
 
-python generate.py -d templates/actor.html -i actor -o ghpages/actor.html
-cp -R data/actor ghpages/data/
+python templates/make_page.py -t templates/page.template -d templates/descriptions/actor.html -i actor -o ghpages/actor.html
+cp -vR data/actor ghpages/data/
 
-python generate.py -d templates/index.html -i repo -o ghpages/index.html
-cp -R data/repo ghpages/data/
+python templates/make_page.py -t templates/page.template -d templates/descriptions/index.html -i repo -o ghpages/index.html
+cp -vR data/repo ghpages/data/
 
-python generate.py -d templates/python.html -i python -o ghpages/python.html
-cp -R data/python ghpages/data/
+python templates/make_page.py -t templates/page.template -d templates/descriptions/python.html -i python -o ghpages/python.html
+cp -vR data/python ghpages/data/
 
-python generate.py -d templates/ruby.html -i ruby -o ghpages/ruby.html
-cp -R data/ruby ghpages/data/
+python templates/make_page.py -t templates/page.template -d templates/descriptions/ruby.html -i ruby -o ghpages/ruby.html
+cp -vR data/ruby ghpages/data/
--- templates/make_page.py
+++ templates/make_page.py
@@ -5,15 +5,17 @@ def get_argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--desc", required=True, help="description html")
     parser.add_argument("-i", "--id", required=True, help="results id")
+    parser.add_argument("-t", "--template", required=True, help="template for a page")
     parser.add_argument("-o", "--out", required=True, help="write destination")
     return parser
 
-def generate(description_filename, data_id, output_filename):
+def generate(template_filename, description_filename, data_id, output_filename):
+    print "processing:", template_filename, description_filename, data_id, output_filename
     data_description = "none"
     with open (description_filename, "r") as description_file:
         data_description = description_file.read()
 
-    with open ("html.template", "r") as template_file:
+    with open (template_filename, "r") as template_file:
         template = string.Template(template_file.read()) 
 
     result = template.safe_substitute(data_description=data_description, data_id=data_id)
@@ -24,4 +26,4 @@ def generate(description_filename, data_id, output_filename):
 if __name__ == '__main__':
     parser = get_argument_parser()
     args = parser.parse_args()
-    generate(args.desc, args.id, args.out)
+    generate(args.template, args.desc, args.id, args.out)
