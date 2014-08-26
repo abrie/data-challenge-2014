2fe93250a07d6644b59dd2b8b646f74bfef3db40
abrie <abrie@>
2014-08-26 21:05:12 +0100

    refactor: DRY the sh scripts with functions

--- collect.sh
+++ collect.sh
@@ -4,10 +4,15 @@ if [ $# -lt 1 ] ; then
 fi
 
 PROJECTID=$1
-python main.py -p $PROJECTID -i actor --model sql/actor-model.sql --state sql/actor-state.sql
-python main.py -p $PROEJCTID -i repo --model sql/repo-model.sql --state sql/repo-state.sql
-python main.py -p $PROJECTID -i python --model sql/python-repo-model.sql --state sql/python-repo-state.sql
-python main.py -p $PROJECTID -i ruby --model sql/ruby-repo-model.sql --state sql/ruby-repo-state.sql
-python main.py -p $PROJECTID -i dcpu16 --model sql/dcpu16-repo-model.sql --state sql/dcpu16-repo-state.sql
-python main.py -p $PROJECTID -i friday --model sql/friday-repo-model.sql --state sql/friday-repo-state.sql
-python main.py -p $PROJECTID -i sunday --model sql/sunday-repo-model.sql --state sql/sunday-repo-state.sql
+
+function collect {
+    python main.py -p $PROJECTID -i $1 --model sql/$1-model.sql --state sql/$1-state.sql
+}
+
+collect actor
+collect repo
+collect python-repo
+collect ruby-repo
+collect dcpu16-repo
+collect friday-repo
+collect sunday-repo
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 DEST="gh-pages"
 mkdir -p $DEST/data
 
@@ -6,23 +7,21 @@ cp -R pages/lib $DEST
 cp pages/main.js $DEST
 cp pages/page.css $DEST
 
-python pages/make_page.py -t pages/page.html -d pages/descriptions/actor.html -i actor -o $DEST/actor.html
-cp -R data/actor $DEST/data/
+function deploy {
+    if [ $# -lt "2" ]; then
+        FILENAME=$1
+    else
+        FILENAME=$2
+    fi
 
-python pages/make_page.py -t pages/page.html -d pages/descriptions/repo.html -i repo -o $DEST/index.html
-cp -R data/repo $DEST/data/
+    python pages/make_page.py -t pages/page.html -d pages/descriptions/$1.html -i $1 -o $DEST/$FILENAME.html
+    cp -R data/$1 $DEST/data/
+}
 
-python pages/make_page.py -t pages/page.html -d pages/descriptions/python.html -i python -o $DEST/python.html
-cp -R data/python $DEST/data/
-
-python pages/make_page.py -t pages/page.html -d pages/descriptions/ruby.html -i ruby -o $DEST/ruby.html
-cp -R data/ruby $DEST/data/
-
-python pages/make_page.py -t pages/page.html -d pages/descriptions/dcpu16.html -i dcpu16 -o $DEST/dcpu16.html
-cp -R data/dcpu16 $DEST/data/
-
-python pages/make_page.py -t pages/page.html -d pages/descriptions/sunday-repo.html -i sunday -o $DEST/sunday-repo.html
-cp -R data/sunday $DEST/data/
-
-python pages/make_page.py -t pages/page.html -d pages/descriptions/friday-repo.html -i friday -o $DEST/friday-repo.html
-cp -R data/friday $DEST/data/
+deploy actor 
+deploy repo index 
+deploy python-repo
+deploy ruby-repo
+deploy dcpu16-repo
+deploy friday-repo
+deploy sunday-repo
